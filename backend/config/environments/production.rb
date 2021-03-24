247ef0300141e2b6acc91b208fd5b37b4e026c11
Luu Nguyen <nnluukhtn@gmail.com>
2021-03-24 18:43:09 +0700

    [Backend] Add config for devise mailer

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -85,6 +85,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { host: ENV['DEFAULT_URL_HOST'] }
+
+  # Config mailer SMTP settings
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV['SMTP_ADDRESS'],
+    port: ENV['SMTP_PORT'],
+    user_name: ENV['SMTP_USER'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
